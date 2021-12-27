cb8790da5f63a128fdf0e96bfbe1c778347e4d1a
Tausif Shekh <nisargcontact2020@gmail.com>
2021-12-27 14:44:04 +0530

    Axios code

---
+++ React-coding/src/coding-question/Axios.js
@@ -0,0 +1,27 @@
+axios
+  .post("/register", {
+    firstName: "Fred",
+    lastName: "Flintstone",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  })
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+// try catch in Action.js File of redux
+//here dispatch is used to call the action and userData as a props
+try {
+  dispatch({ type: "REGISTER_USER_REQUEST" });
+  const config = { headers: { "Content-Type": "multipart/form-data" } };
+  axios.post("/register", userData, config);
+} catch (error) {
+  dispatch({
+    type: "REGISTER_USER_FAILURE",
+    payload: error.response.data.message,
+  });
+}
